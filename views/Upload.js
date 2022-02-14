f2ee61f0e5b35ad350d4b5653096d3e5d47dee23
Sirja Kosonen <sirja.kosonen@gmail.com>
2022-02-14 10:32:16 +0200

    Create Upload.js

---
+++ views/Upload.js
@@ -0,0 +1,12 @@
+import { Container, Text } from "native-base";
+import React from "react";
+
+function Upload() {
+  return (
+    <Container>
+      <Text mt="3">Just a test.</Text>
+    </Container>
+  );
+}
+
+export default Upload;
