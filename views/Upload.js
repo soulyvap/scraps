90adcf181122d577a84e237516d0ea855cdaa593
Sirja Kosonen <sirja.kosonen@gmail.com>
2022-02-15 16:11:21 +0200

    Added most of UI elements
    Still missing:
    DateTimePicker, add tags, post button, bottom navigation
    
    Still in progress:
    Selecting category

--- views/Upload.js
+++ views/Upload.js
@@ -1,12 +1,4 @@
-import {
-  Box,
-  extendTheme,
-  Heading,
-  Input,
-  NativeBaseProvider,
-  ScrollView,
-  TextArea,
-} from "native-base";
+import { Box, extendTheme, NativeBaseProvider, ScrollView } from "native-base";
 import { StyleSheet } from "react-native";
 import PostForm from "../components/PostForm";
 
@@ -14,14 +6,12 @@ const Upload = () => {
   const theme = extendTheme({
     components: {
       Input: {
-        variants: {
-          test: () => {
-            return {
-              bg: "#F9F4F1",
-              borderRadius: 15,
-              borderWidth: 1,
-            };
-          },
+        baseStyle: {
+          bgColor: "#F9F4F1",
+          borderColor: "transparent",
+          borderRadius: 15,
+          marginBottom: "5%",
+          textAlign: "center",
         },
       },
     },
@@ -30,39 +20,8 @@ const Upload = () => {
   return (
     <NativeBaseProvider theme={theme}>
       <Box flex="1" safeAreaTop>
-        <ScrollView px="5%">
-          <Heading size="md" style={styles.title}>
-            Add title
-          </Heading>
-          <Input
-            size="lg"
-            variant="test"
-            width="100%"
-            placeholder="Green salad"
-            alignSelf="center"
-            px={4}
-          />
-          <Heading size="md" style={[styles.field, styles.title]}>
-            Select latest pick-up date
-          </Heading>
-          <Heading size="md">Add description</Heading>
-          <TextArea
-            placeholder="Testing"
-            width="100%"
-            py={5}
-            style={styles.field}
-          />
+        <ScrollView px="5%" bgColor={"white"}>
           <PostForm />
-          <Input
-            size="lg"
-            variant="test"
-            multiline={true}
-            height={100}
-            width="100%"
-            placeholder="Green salad"
-            alignSelf="center"
-            px={4}
-          />
         </ScrollView>
       </Box>
     </NativeBaseProvider>
@@ -75,7 +34,6 @@ const styles = StyleSheet.create({
     marginBottom: "2%",
   },
   field: {
-    backgroundColor: "#F9F4F1",
     marginTop: "5%",
   },
 });
